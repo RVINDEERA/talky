f7143c374852ab152c7feb9e0bcb8a22a295cffd
RVINDEERA <rdasbandara@gmail.com>
2024-01-31 21:38:16 +0530

    Update client

--- src/main/java/lk/ijse/talky/client/Client.java
+++ src/main/java/lk/ijse/talky/client/Client.java
@@ -1,4 +1,110 @@
 package lk.ijse.talky.client;
 
-public class Client {
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import lk.ijse.talky.controller.ChatFormController;
+import lk.ijse.talky.dto.UserDto;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.Serializable;
+import java.net.Socket;
+
+public class Client implements Runnable , Serializable {
+    private final String name;
+
+    public String getName() {
+        return name;
+    }
+
+    private final Socket socket;
+    private final DataInputStream dataInputStream;
+    private final DataOutputStream dataOutputStream;
+    private ChatFormController chatFormController;
+
+    public Client(String name) throws IOException {
+        this.name=name;
+//        if(pw==null){
+//        }else{
+//            this.pw=pw;
+//        }
+        this.socket = new Socket("localhost", 1200);
+        this.dataInputStream = new DataInputStream(socket.getInputStream());
+        this.dataOutputStream = new DataOutputStream(socket.getOutputStream());
+        dataOutputStream.writeUTF(name);
+        dataOutputStream.flush();
+
+        try {
+            loadScene();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+    private void loadScene() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/chat_form.fxml"));
+        AnchorPane anchorPane = loader.load();
+        Scene scene = new Scene(anchorPane);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.centerOnScreen();
+        stage.setTitle(name+ "'s Talky");
+        stage.setAlwaysOnTop(true);
+        stage.setResizable(false);
+        stage.show();
+
+        stage.setOnCloseRequest(event -> {
+            try{
+                dataInputStream.close();
+                dataOutputStream.close();
+                socket.close();
+            }catch (IOException e){
+                System.out.println(e);
+            }
+        });
+    }
+
+    @Override
+    public void run() {
+        String message = "";
+        while(!message.equals("exit")){
+            try{
+                message = dataInputStream.readUTF();
+                if(message.equals("*image*")){
+                    recieveImage();
+                }else{
+                    chatFormController.writeMessage(message);
+                }
+            }catch (IOException e){
+                try {
+                    socket.close();
+                }catch (IOException ex){
+                    System.out.println(ex);
+                }
+            }
+        }
+    }
+
+    public void sendImage(byte[] bytes) throws IOException {
+        dataOutputStream.writeUTF("*image*");
+        dataOutputStream.writeInt(bytes.length);
+        dataOutputStream.write(bytes);
+        dataOutputStream.flush();
+    }
+
+
+
+
+    private void recieveImage() throws IOException {
+        String message = dataInputStream.readUTF();
+        int size = dataInputStream.readInt();
+        byte[] bytes = new byte[size];
+        dataInputStream.readFully(bytes);
+        System.out.println(name+"- Image Recieved from "+ message);
+        chatFormController.setImage(bytes,message);
+    }
+
+
 }
--- src/main/java/lk/ijse/talky/controller/ChatFormController.java
+++ src/main/java/lk/ijse/talky/controller/ChatFormController.java
@@ -1,14 +1,22 @@
 package lk.ijse.talky.controller;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import lk.ijse.talky.client.Client;
 import lk.ijse.talky.dto.UserDto;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 
 public class ChatFormController {
     @FXML
@@ -27,14 +35,17 @@ public class ChatFormController {
     @FXML
     private ScrollPane scrollPane;
 
+
     @FXML
     private TextField txtTypemsg;
     private UserDto userDto;
     private LoginFormController loginFormController;
+    private Client client;
 
     public void initialize(){
         lblName.setText(LoginFormController.name);
     }
+
     @FXML
     void btnSendEmojiOnActioni(ActionEvent event) {
 
@@ -53,6 +64,33 @@ public class ChatFormController {
     @FXML
     void txtTypingOnAction(ActionEvent event) {
 
+    }
+
+    public void setImage(byte[] bytes,String sender){
+        HBox hBox = new HBox();
+        Label messageLbl = new Label(sender);
+        messageLbl.setStyle("-fx-background-color:   #f46200;-fx-background-radius:15;-fx-font-size: 18;-fx-font-weight: normal;-fx-text-fill: white;-fx-wrap-text: true;-fx-alignment: center;-fx-content-display: left;-fx-padding: 10;-fx-max-width: 350;");
+
+        hBox.setStyle("-fx-fill-height: true; -fx-min-height: 50; -fx-pref-width: 520; -fx-max-width: 520; -fx-padding: 10; " + (sender.equals(client.getName()) ? "-fx-alignment: center-right;" : "-fx-alignment: center-left;"));
+        Platform.runLater(() -> {
+            ImageView imageView = new ImageView(new Image(new ByteArrayInputStream(bytes)));
+            imageView.setStyle("-fx-padding: 10px;");
+            imageView.setFitHeight(180);
+            imageView.setFitWidth(100);
+
+            hBox.getChildren().addAll(messageLbl, imageView);
+            vBox.getChildren().add(hBox);
+        });
+    }
+
+    public void writeMessage(String message) {
+        //print msg on other clients
+        HBox hBox = new HBox();
+        hBox.setStyle("-fx-alignment: center-left;-fx-fill-height: true;-fx-min-height: 50;-fx-pref-width: 520;-fx-max-width: 520;-fx-padding: 10");
+        Label messageLbl = new Label(message);
+        messageLbl.setStyle("-fx-background-color:   #648BB0;-fx-background-radius:15;-fx-font-size: 18;-fx-font-weight: normal;-fx-text-fill: white;-fx-wrap-text: true;-fx-alignment: center-left;-fx-content-display: left;-fx-padding: 10;-fx-max-width: 350;");
+        hBox.getChildren().add(messageLbl);
+        Platform.runLater(() -> vBox.getChildren().add(hBox));
     }
 
 }
